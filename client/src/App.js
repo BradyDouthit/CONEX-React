1710a10261e01d3627b3e398ec33bd926e571b12
Brady Douthit <brady.douthit99@gmail.com>
2020-07-26 12:47:15 -0400

    add basic mern stack structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import axios from 'axios';
-//components
-import SystemIndex from './components/SystemIndex/SystemIndex';
-import HomePage from './components/HomePage/HomePage'
+import logo from './logo.svg';
+import './App.css';
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Route 
-          exact path="/"
-          render={() => <SystemIndex title="System Index" />}
-        />
-        <Route 
-          path="/home"
-          render={() =>  <HomePage />}
-        />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
