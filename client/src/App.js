9d23969fbad7a78c63439fa2237593323ad91523
Brady Douthit <brady.douthit99@gmail.com>
2020-07-27 18:06:47 -0400

    change App component to class

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,32 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
 
-  axios.get('/api/test')
-  .then(response => {
-    console.log(response)
-  })
-  .catch(error => {
-    console.log(error)
-  })
+    }
+  };
 
-  return (
-    <div>
-      <h1>This is everything working</h1>
-    </div>
-  );
+  componentDidMount() {
+    axios.get('/api/test')
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>This is everything working</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
